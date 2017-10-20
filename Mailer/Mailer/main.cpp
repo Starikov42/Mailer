5c6a8e32cf2f6fa154977f8c021bd19510a1ddd0
Michael Starikov <starikovmishaandrey@gmail.com>
2017-10-20 19:35:55 +0300

    Начало проекта

---
+++ Mailer/Mailer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mailer
+//
+//  Created by Михаил Стариков on 20.10.17.
+//  Copyright © 2017 Михаил Стариков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
